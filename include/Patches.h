95763e33fc7c36eeda9494a21670104d80722ac8
SkyHorizon3 <neuner.r@gmx.de>
2024-01-29 21:19:45 +0100

    Add AE support

--- include/Patches.h
+++ include/Patches.h
@@ -10,7 +10,7 @@ namespace SDF
 		{
 			const AddStaggerActorPatch fix{};
 			const auto funcAddr = REL::RelocationID(34188, 34982).address();  //1.5.97 140563460		//1.6.1170 1405D41B0
-			DKUtil::Hook::WritePatch(funcAddr + REL::Relocate(0x69, 0x69), &fix);
+			DKUtil::Hook::WritePatch(funcAddr + REL::Relocate(0x69, 0x69, 0x69), &fix);
 			INFO("{} Done!", __FUNCTION__);
 		}
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,20 @@
 
 DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
+	/*
 #ifndef NDEBUG
 	while (!IsDebuggerPresent()) {
 		Sleep(100);
 	}
 #endif
+	*/
 
 	DKUtil::Logger::Init(Plugin::NAME, REL::Module::get().version().string());
 
 	//REL::Module::reset();
 	SKSE::Init(a_skse);
 
-	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
+	INFO("{} v{} loaded", Plugin::NAME, Plugin::VERSION);
 
 	// do stuff
 	SDF::AddStaggerActorPatch::Install();
@@ -34,7 +36,25 @@ DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 				ERROR("Redundant plugin \"{}\" Loaded, please delete the redundant mod first!", redundantDLLName);
 			}
 		}
-	});
+		});
 
+	return true;
+}
+
+extern "C" DLLEXPORT const auto SKSEPlugin_Version = []() noexcept {
+	SKSE::PluginVersionData v;
+	v.PluginName(Plugin::NAME.data());
+	v.PluginVersion(Plugin::VERSION);
+	v.UsesAddressLibrary(true);
+	v.HasNoStructUse();
+	return v;
+	}
+();
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo * pluginInfo)
+{
+	pluginInfo->name = SKSEPlugin_Version.pluginName;
+	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
+	pluginInfo->version = SKSEPlugin_Version.pluginVersion;
 	return true;
 }
